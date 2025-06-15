fdeee4bc7af39f41173d6d790aaa3a553dbd6629
Phichayut 'Florentin' Sakwiset <florentin@wemake.cx>
2025-06-15 14:55:34 +0200

    refactor(astro-config): remove experimental manualChunks option

--- packages/clients/wh/university-submission-website/astro.config.ts
+++ packages/clients/wh/university-submission-website/astro.config.ts
@@ -17,9 +17,6 @@ export default defineConfig({
   output: "hybrid",
   adapter: cloudflare({
     imageService: "compile",
-    experimental: {
-      manualChunks: ["sharp"],
-    },
   }),
   compressHTML: true,
   i18n: {
